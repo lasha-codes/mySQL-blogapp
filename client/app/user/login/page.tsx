e60f5b1d809435f5e7b19a8f9faac7c6b9e64cfc
lasha-codes <lashagiorgi420@gmail.com>
2024-03-31 04:22:44 -0700

    made login form controlled also

--- client/app/user/login/page.tsx
+++ client/app/user/login/page.tsx
@@ -1,14 +1,27 @@
+import React, { useState } from 'react'
+
 const Login = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
   return (
     <main className='w-full h-screen flex justify-center flex-col gap-10 items-center bg-main'>
       <h1 className='text-2xl font-light'>Login</h1>
       <form className='flex flex-col gap-2 w-1/2'>
         <input
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
           className='w-full py-1 px-4 rounded-md'
           placeholder='email'
           required
         />
         <input
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
           className='w-full py-1 px-4 rounded-md'
           placeholder='password'
           required
