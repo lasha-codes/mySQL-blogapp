f5cd3d6ae1c8c7065023f657a7e8a536049be4f1
lasha-codes <lashagiorgi420@gmail.com>
2024-03-31 02:35:02 -0700

    setting um components for the future

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import Posts from './components/Posts'
 
 const App = () => {
   return (
     <main className='w-full bg-main h-screen p-10'>
       <Header />
+      <Posts />
     </main>
   )
 }
